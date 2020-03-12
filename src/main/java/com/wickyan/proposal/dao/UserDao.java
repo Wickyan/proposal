eaddd9a68d1ac1e817dd1a10f5caa0541afc4ff3
wickyan <yanweiqi199888@126.com>
2020-03-12 19:06:11 +0800

    mapperXml

--- src/main/java/com/wickyan/proposal/dao/UserDao.java
+++ src/main/java/com/wickyan/proposal/dao/UserDao.java
@@ -3,12 +3,14 @@ package com.wickyan.proposal.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wickyan.proposal.entity.UserEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * 用户
  * Created by wickyan on 2020/3/10
  */
 @Mapper
+@Repository
 public interface UserDao extends BaseMapper<UserEntity> {
 	
 }
--- src/test/java/com/wickyan/proposal/ProposalApplicationTests.java
+++ src/test/java/com/wickyan/proposal/ProposalApplicationTests.java
@@ -1,6 +1,8 @@
 package com.wickyan.proposal;
 
+import com.wickyan.proposal.dao.UserDao;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +10,12 @@ class ProposalApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+    @Autowired
+    private UserDao userDao;
+    @Test
+    public void testSelectList(){
+
     }
 
 }
