923fbe30ef4f539aec66dac275e6e946ee80dc0b
wickyan <yanweiqi199888@126.com>
2020-03-17 19:30:55 +0800

    publish下拉列表

--- src/main/java/com/wickyan/proposal/controller/PublishController.java
+++ src/main/java/com/wickyan/proposal/controller/PublishController.java
@@ -3,7 +3,9 @@ package com.wickyan.proposal.controller;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.wickyan.proposal.dao.DeptDao;
 import com.wickyan.proposal.dao.TopicDao;
+import com.wickyan.proposal.entity.DeptEntity;
 import com.wickyan.proposal.entity.TopicEntity;
+import com.wickyan.proposal.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by wickyan on 2020/3/14
@@ -29,26 +35,44 @@ public class PublishController {
         return "new";
     }
     @GetMapping("/publish")
-    public String topic(Model model) {
+    public String publish(Model model,
+                        HttpSession session,
+                        Map<String, Object> map) {
+        Object userEntity = session.getAttribute("userEntity");
+        if (null == userEntity) {
+            map.put("msg", "您还没有登录");
+            return "login";        //页面错误跳转回页面，并写入msg
+        }
+        List<DeptEntity> deptEntities = deptDao.selectList(null);
+        model.addAttribute("deptEntities", deptEntities);
         return "publish";
     }
+
+
     @PostMapping("/publish")
-    public String pushTopic(
+    public String doPublish(
             @RequestParam(value = "topicTitle", required = false) String topicTitle,
             @RequestParam(value = "topicText", required = false) String topicText,
             @RequestParam(value = "deptId", required = false) Long deptId,
+            Model model,
+            HttpSession session) {
+        UserEntity userEntity = (UserEntity)session.getAttribute("userEntity");
 
-            Model model) {
         TopicEntity topicEntity = new TopicEntity();
         topicEntity.setTopicTitle(topicTitle);
         topicEntity.setTopicText(topicText);
         topicEntity.setDeptId(deptId);
+        topicEntity.setUserId(userEntity.getUserId());
         System.out.println(topicEntity);
 
+        List<DeptEntity> deptEntities = deptDao.selectList(null);
+        model.addAttribute("deptEntities", deptEntities);
 
         model.addAttribute("topicTitle", topicTitle);
         model.addAttribute("topicText", topicText);
         model.addAttribute("deptId", deptId);
+
+
         if (StringUtils.isBlank(topicTitle)) {
             model.addAttribute("error", "标题不能为空");
             return "publish";
